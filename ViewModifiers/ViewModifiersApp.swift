344d1f7cb3b85a3dcbbe0e9c8221d4abe6f53f8d
Jovins <jovinscoder@gmail.com>
2025-05-08 11:45:56 +0800

    create pro

---
+++ ViewModifiers/ViewModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifiersApp.swift
+//  ViewModifiers
+//
+//  Created by Jovins on 2025/5/8.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
